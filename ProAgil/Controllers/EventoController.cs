a6a5f001c0cd58babd7aaef3d4736370a852c33e
bilhares <felipe.bilhares@hotmail.com>
2020-07-01 11:15:46 -0400

    projeto

---
+++ ProAgil/Controllers/EventoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Data;
+using ProAgil.Model;
+
+namespace ProAgil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoController : ControllerBase
+    {
+        public DataContext _context { get; }
+
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados FALHOU");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoID == id);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados FALHOU");
+            }
+        }
+    }
+}
---
+++ ProAgil/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProAgil.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=localhost\\SQLEXPRESS;initial catalog=PRO_AGIL;persist security info=True;user id=sa;password=root;MultipleActiveResultSets=True;App=EntityFramework");
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
