11cea9622ac726344b260ff31278a92781c2fdfe
bilhares <felipe.bilhares@hotmail.com>
2020-07-09 11:42:03 -0400

    configurando o identity

---
+++ ProAgil.Domain/Identity/Roles.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAgil.Domain.Identity
+{
+    public class Roles : IdentityRole<int>
+    {
+        public List<UserRoles> UserRoles { get; set; }
+    }
+}
--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +11,35 @@ using System.Threading.Tasks;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext : DbContext
+    public class ProAgilContext : IdentityDbContext<User, Roles, int, IdentityUserClaim<int>,
+                                                    UserRoles, IdentityUserLogin<int>, IdentityRoleClaim<int>,
+                                                    IdentityUserToken<int>>
     {
         public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) { }
         public DbSet<Evento> Eventos { get; set; }
-        public DbSet<Palestrante> Palestrantes{ get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
         public DbSet<PalestranteEvento> PalestranteEventos { get; set; }
-        public DbSet<Lote> Lotes{ get; set; }
-        public DbSet<RedeSocial> redeSocials{ get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<RedeSocial> redeSocials { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserRoles>(userRoles =>
+                {
+                    userRoles.HasKey(ur => new { ur.UserId, ur.RoleId });
+                    userRoles.HasOne(ur => ur.Roles)
+                            .WithMany(r => r.UserRoles)
+                            .HasForeignKey(ur => ur.RoleId)
+                            .IsRequired();
+
+                    userRoles.HasOne(ur => ur.User)
+                           .WithMany(r => r.UserRoles)
+                           .HasForeignKey(ur => ur.UserId)
+                           .IsRequired();
+                }
+            );
+
             modelBuilder.Entity<PalestranteEvento>().HasKey(pe => new { pe.EventoId, pe.PalestranteId });
 
         }
